e4895018a0d6877a49faa2eb28b936184b7232df
tsekhanskaya <anutta2222@gmail.com>
2023-03-22 17:31:04 +0100

    Refactoring. Connection to database

--- task1/classes/database.py
+++ task1/classes/database.py
@@ -2,6 +2,7 @@ import json
 import xml.etree.ElementTree as ET
 import psycopg2
 import logging
+import typing
 import os
 
 from dotenv import load_dotenv
@@ -10,22 +11,32 @@ load_dotenv()
 
 
 class Database:
+
+    _connection: typing.Union[psycopg2.connect, None] = None
+
+    @property
+    def connection(self) -> psycopg2.connect:
+        if self._connection is None:
+            try:
+                type(self)._connection = psycopg2.connect(
+                    host='localhost',
+                    database='training',
+                    user=os.getenv('DATABASE_USER'),
+                    password=os.getenv('DATABASE_PASSWORD'))
+            except Exception as e:
+                logging.error(e)
+        return self._connection
+
     def __init__(self):
         try:
-            self.conn = psycopg2.connect(
-                host='localhost',
-                database='training',
-                user=os.getenv('DATABASE_USER'),
-                password=os.getenv('DATABASE_PASSWORD'))
-            logging.info('Created the connection object')
-
-            self.cursor = self.conn.cursor()
-            logging.info('Created the cursor object')
 
             query_files = ['create_table_rooms.sql', 'create_table_students.sql', 'indexes.sql']
             for query_file in query_files:
                 query_create_table = self.query_string(query_file)
-                self.cursor.execute(query_create_table)
+                with self.connection.cursor() as cursor:
+                    cursor.execute(query_create_table)
+                self.connection.commit()
+
                 logging.info(f' {query_file} created successfully')
                 print(f' {query_file} created successfully')
 
@@ -39,9 +50,8 @@ class Database:
         :return: None
         """
         try:
-            self.conn.commit()
-            self.cursor.close()
-            self.conn.close()
+            self._connection.close()
+            type(self)._connection = None
             logging.info('Database disconnection was successful.')
         except Exception as e:
             logging.exception("Error: %s", e)
@@ -55,17 +65,18 @@ class Database:
         try:
             rows = [(student['id'], student['name'], student['sex'], student['birthday'], student['room']) for student
                     in students]
-            self.cursor.executemany(
-                "INSERT INTO students (id, name, sex, birthday, room_id) "
-                "VALUES (%s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING",
-                rows
-            )
-            self.conn.commit()
-            inserted_count = self.cursor.rowcount
+            with self.connection.cursor() as cursor:
+                cursor.executemany(
+                    "INSERT INTO students (id, name, sex, birthday, room_id) "
+                    "VALUES (%s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING",
+                    rows
+                )
+                self.connection.commit()
+                inserted_count = cursor.rowcount
             print(f"{inserted_count} students were uploaded successfully!")
             logging.info(f'{inserted_count} students uploaded to students table!')
         except Exception as e:
-            self.conn.rollback()
+            self.connection.rollback()
             logging.exception("Error: %s", e)
 
     def write_rooms(self, rooms: list) -> None:
@@ -76,15 +87,16 @@ class Database:
         """
         try:
             rows = [(room['id'], room['name']) for room in rooms]
-            self.cursor.executemany(
-                "INSERT INTO rooms (id, name) VALUES (%s, %s) ON CONFLICT (id) DO NOTHING",
-                rows
-            )
-            self.conn.commit()
-            inserted_count = self.cursor.rowcount
-            print(f"{inserted_count} rooms were uploaded!")
+            with self.connection.cursor() as cursor:
+                cursor.executemany(
+                    "INSERT INTO rooms (id, name) VALUES (%s, %s) ON CONFLICT (id) DO NOTHING",
+                    rows
+                )
+                self.connection.commit()
+                inserted_count = cursor.rowcount
+                print(f"{inserted_count} rooms were uploaded!")
         except Exception as e:
-            self.conn.rollback()
+            self.connection.rollback()
             logging.exception(e)
 
     @staticmethod
@@ -109,21 +121,23 @@ class Database:
         """
         try:
             query = Database.query_string(query_filename)
-            self.cursor.execute(query)
-            results = self.cursor.fetchall()
+            with self.connection.cursor() as cursor:
+                cursor.execute(query)
+                results = cursor.fetchall()
+                description = cursor.description
             filename_without_extension = query_filename.replace(".sql", "")
             if not os.path.exists('results'):
                 os.makedirs('results')
             output_file = f"result_{filename_without_extension}.{format_result_file}"
 
             if format_result_file == 'json':
-                self.response_json(results, output_file)
+                self.response_json(results, output_file, description)
             elif format_result_file == 'xml':
-                self.response_xml(results, output_file)
+                self.response_xml(results, output_file, description)
         except Exception as e:
             logging.exception(e)
 
-    def response_json(self, results: list, output_file: str) -> None:
+    def response_json(self, results: list, output_file: str, description) -> None:
         """
         The query result is written to a result_{filename_query}.json.
         :param output_file: str
@@ -133,14 +147,15 @@ class Database:
         try:
             with open(os.path.join('results', output_file), 'w') as f:
                 rows = []
+
                 for row in results:
-                    rows.append(dict(zip([column[0] for column in self.cursor.description], row)))
+                    rows.append(dict(zip([column[0] for column in description], row)))
                 json.dump(rows, f, indent=1)
             logging.info(f"The result was successfully written to a {output_file}!")
         except Exception as e:
             logging.exception(e)
 
-    def response_xml(self, results: list, output_file: str) -> None:
+    def response_xml(self, results: list, output_file: str, description) -> None:
         """
         The query result is written to a result_{filename_query}.xml.
         :param output_file: str
@@ -152,12 +167,14 @@ class Database:
             root = ET.Element('result')
             for row in results:
                 record = ET.SubElement(root, 'record')
-                for i in range(len(self.cursor.description)):
-                    field = self.cursor.description[i][0]
+
+                for i in range(len(description)):
+                    field = description[i][0]
                     value = str(row[i])
                     ET.SubElement(record, field).text = value
             tree = ET.ElementTree(root)
             tree.write(output_path, encoding='utf-8', xml_declaration=True)
             logging.info(f"The result was successfully written to a {output_file}!")
+            print(f"The result was successfully written to a {output_file}!")
         except Exception as e:
             logging.exception(e)
--- task1/classes/file.py
+++ task1/classes/file.py
@@ -4,12 +4,9 @@ import logging
 
 class File:
     def __init__(self, students_file_path: str, rooms_file_path: str):
-        try:
-            self.students_file_path = students_file_path
-            self.rooms_file_path = rooms_file_path
-            logging.info('Created instance class File!')
-        except Exception as e:
-            logging.error(e)
+        self.students_file_path = students_file_path
+        self.rooms_file_path = rooms_file_path
+        logging.info('Created instance class File!')
 
     def read_rooms(self) -> list:
         """
@@ -19,9 +16,9 @@ class File:
         try:
             with open(self.rooms_file_path, 'r') as f:
                 rooms = json.load(f)
-            return rooms
         except Exception as e:
             logging.error(e)
+        return rooms
 
     def read_students(self) -> list:
         """
@@ -31,6 +28,6 @@ class File:
         try:
             with open(self.students_file_path, 'r') as f:
                 students = json.load(f)
-            return students
         except Exception as e:
             logging.error(e)
+        return students
