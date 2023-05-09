7262b9cba6e263ef3368437597d0caa2b6cf068b
tsekhanskaya <anutta2222@gmail.com>
2023-05-09 21:12:26 +0300

    Added the solution

---
+++ task8/dags/DDL/create_master_table.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE TABLE MASTER_TABLE (
+    _id VARCHAR,
+    IOS_App_Id INT,
+    Title VARCHAR,
+    Developer_Name VARCHAR,
+    Developer_IOS_Id FLOAT,
+    IOS_Store_Url VARCHAR,
+    Seller_Official_Website VARCHAR,
+    Age_Rating VARCHAR,
+    Total_Average_Rating FLOAT,
+    Total_Number_of_Ratings FLOAT,
+    Average_Rating_For_Version FLOAT,
+    Number_of_Ratings_For_Version INT,
+    Original_Release_Date DATE,
+    Current_Version_Release_Date DATE,
+    Price_USD FLOAT,
+    Primary_Genre VARCHAR,
+    All_Genres VARCHAR,
+    Languages VARCHAR,
+    Description VARCHAR
+);
